c2e71aada272f91f79b6dc45e39830f54769b1c1
decimo3 <ruanfaustinocamellodasilva@gmail.com>
2020-09-09 21:57:02 -0300

    SUCCESS: API consumption working perfectly!

--- src/components/publicacoes.vue
+++ src/components/publicacoes.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <criarPublicacao />
-    <div v-if="!self.isLoading">
-      <div v-for="(depoimento, index) in self.fodase" :key="index">
+    <div v-if="!isLoading">
+      <div v-for="(depoimento, index) in depoimentos" :key="index">
         <listarPublicacao :depoimento="depoimento" />
       </div>
     </div>
@@ -14,14 +14,15 @@ import listarPublicacao from "./post.handler/list.post.vue";
 export default {
   data() {
     return {
-      self: this
+      isLoading: true,
+      depoimentos: Array
     };
   },
   components: {
     criarPublicacao,
     listarPublicacao,
   },
-    created: async () => {
+    created: async function () {
     console.log("Estou sendo criado !");
     const zelda = `http://localhost:3000/api/v1/publicacoes/`;
     await fetch(zelda, { method: "GET" }).then((response) => {
@@ -29,8 +30,8 @@ export default {
         response.json().then((json) => {
           console.log("Fetch bem sucedido!");
           console.log(json);
-          self.fodase = json
-          self.isLoading = false
+          this.depoimentos = json
+          this.isLoading = false
         });
       } else {
         console.log("Response is not OK");
