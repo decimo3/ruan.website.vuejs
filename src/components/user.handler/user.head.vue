74254e9a3bf88ffa77b88312e62d5cc08b58cc3f
decimo3 <ruanfaustinocamellodasilva@gmail.com>
2020-09-24 22:25:21 -0300

    WIP: user.head layout updates part 1

--- src/components/user.handler/user.head.vue
+++ src/components/user.handler/user.head.vue
@@ -1,12 +1,8 @@
 <template>
-  <div class="linha folder">
-    <div class="usuario coluna-12" v-if="usuario.logado === false">
-      Anônimo - Para escrever um depoimento ou jogar, <a @click="mudarPagina(5)">faça login</a> ou <a @click="mudarPagina(6)">cadastre-se</a>.
-    </div>
-    <div class="usuario coluna-12 logado" v-else>
-      Seja bem vindo {{usuario.nome}}. Poder {{usuario.poder}} <a>minha conta</a> - <a>sair</a><br/>
-      Experiência: {{usuario.score}}pts. Moedas: {{usuario.moedas.gold}}G {{usuario.moedas.silver}}S {{usuario.moedas.bronze}}B
-    </div>
+  <div class="linha folder"> <!-- folder -->
+    <div class="usuario coluna-12" v-if="usuario.logado === false">Anônimo - Para escrever um depoimento ou jogar, <a @click="mudarPagina(5)">faça login</a> ou <a @click="mudarPagina(6)">cadastre-se</a>.</div>
+    <div class="usuario coluna-12 logado" v-if="usuario.logado === true">Seja bem vindo {{usuario.nome}}. Poder {{usuario.poder()}} <a>minha conta</a> - <a>sair</a></div>
+    <div class="usuario coluna-12 logado" v-if="usuario.logado === true">Experiência: {{usuario.score}}pts. Moedas: {{usuario.moedas.gold}}G {{usuario.moedas.silver}}S {{usuario.moedas.bronze}}B</div>
   </div>
 </template>
 <script>
@@ -20,7 +16,7 @@ export default {
         forca: 30,
         genio: 5,
         agil: 12,
-        power: function () {
+        poder: function () {
           return this.forca + this.genio + this.agil
         },
         moedas: {
@@ -69,15 +65,13 @@ export default {
   .folder {
     border: solid 2px #000000;
     padding: 1px;
-    height: 70px;
+    height: 100px;
     text-align: right;
   }
   .mensagem {
     text-align: left;
-    height: 100%;
   }
   .usuario {
-    height: 100%;
 
   }
 }
