0fe6fe0ac874426230b6def268f1c39f15e19b56
decimo3 <ruanfaustinocamellodasilva@gmail.com>
2020-09-27 15:46:55 -0300

    DONE: Validation login work! TODO: Send login to API

--- src/components/user.handler/login.user.vue
+++ src/components/user.handler/login.user.vue
@@ -2,11 +2,12 @@
   <form class="linha cadastro">
     <h2 class="shadow">Entre com a sua conta</h2>
     <label for="email">E-mail:</label>
-    <input type="email" id="email" name="email" placeholder="Seu e-mail aqui!" :value="dados.email" required />
+    <input type="email" id="email" name="email" placeholder="Seu e-mail aqui!" v-model="dados.email" required />
     <br />
     <label for="senha">Senha:</label>
-    <input type="password" name="senha" id="senha" placeholder="Sua senha aqui!" :value="dados.senha" required />
+    <input type="password" name="senha" id="senha" placeholder="Sua senha aqui!" v-model="dados.senha" required />
     <br />
+    <div class="isInvalid" v-if="isInvalid">E-mail or password is invalid!</div>
     <br />
     <input type="submit" id="entrar" name="entrar" class="botão" @click="logarUsuario($event)" value="Entrar"/>
     <br />
@@ -19,20 +20,48 @@
 export default {
   data () {
     return {
+      isInvalid: false,
       dados: {
-        email: "dasdasdadas",
-        senha: "asdadadasdas"
+        email: "",
+        senha: ""
       }
     }
   },
   methods: {
+    validateLogin: function (login, parafrase) {
+      if (this.validEmail(login)) {
+        if (this.validParafrase(parafrase)) {
+          return true
+        } else {
+          return false
+        }
+      } else {
+        return false
+      }
+    },
+    validParafrase: function (senha) {
+      if (senha.length < 8) {
+        return false
+      } else {
+        return true
+      }
+    },
+    validEmail: function (email) {
+      var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return regex.test(email);
+    },
     logarUsuario: function ($event) {
       $event.preventDefault()
-      console.log("Logando usuário...")
-      console.log(this.dados.email, this.dados.senha)
-      this.email = this.senha = ""
+      if (this.validateLogin(this.dados.email, this.dados.senha)) {
+        console.log(this.dados.email, this.dados.senha)
+        this.$emit("logar", this.dados)
+        this.isInvalid = false
+        this.dados.email = this.dados.senha = ""
+      } else {
+        this.isInvalid = true
+      }
     }
-  }
+  } // Close methods
 };
 </script>
 <style scoped>
@@ -51,5 +80,10 @@ export default {
 }
 a {
   text-decoration: underline;
+}
+.isInvalid {
+  color: #ff0000;
+  padding: 5px;
+  padding-top: 10px;
 }
 </style>
